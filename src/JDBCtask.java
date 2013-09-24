1e92cef0efdfb3548f8bf687d9a499766185dc4b
Даша и Паша <  @adara-pasha>
2013-09-24 23:04:38 +0300

    Привет, Мартин добавил комментарии к твоей проге. Поправь, пожалуйста. насколько я вижу, ты не совсем понимаешь, что вообще делается в программе. Если что - могу объяснить смысл отдельных выражений. Пиши и звони

--- src/JDBCtask.java
+++ src/JDBCtask.java
@@ -10,13 +10,13 @@ public class JDBCtask{
  String dbName = "jdbctutorial";
  String driverName = "com.mysql.jdbc.Driver";
  String userName = "root";
- String password = "root";
+ String password = "Matrix1988";
  PreparedStatement preparedstatement=null;
  ResultSet rs;
  try{
   Class.forName(driverName).newInstance();
-  con = DriverManager.getConnection(url, userName, password);
- //con = DriverManager.getConnection(url+dbName, userName, password);
+  //con = DriverManager.getConnection(url, userName, password);
+ con = DriverManager.getConnection(url, userName, password);
   Statement st = con.createStatement();
   String table =
 		  "CREATE TABLE Film(year integer,film String,Director String,Country String)";
@@ -29,11 +29,15 @@ public class JDBCtask{
 		  System.out.println("Table creation process successfully!");
 
   
-  st.executeUpdate(table);
-  System.out.println("Table creation process successfully!");
+  //st.executeUpdate(table);
+  System.out.println("Table creation process successfully!");   // не нужна строка
   }
   catch(SQLException s){
-  st.executeUpdate(table);	
+  st.executeUpdate(table);	  // запуска€ программу по второму разу получаю java.sql.SQLException: No database selected
+                              // тебе нужно добавить подключение к базе данных,
+                              // ј то, ты пытаешьс€ создать таблицу, когда ты не подключен к базе
+                              // Ѕолее того - така€ таблица у теб€ уже создана! 
+                              // ¬ообще не понимаю, зачем это тут
   System.out.println("Table allready exists!");
   }
  preparedstatement = con
@@ -49,7 +53,8 @@ x=Sc.nextInt();
 if (x==1){
 	Statement stmt = con.createStatement();
 
-rs = stmt.executeQuery("SELECT film FROM Film WHERE Year = 2010");		
+rs = stmt.executeQuery("SELECT film FROM Film WHERE Year = 2010");		// java.sql.SQLException: No database selected
+                                                                        // “ут нужно доваить подключение к базе данных
 																								
 while ( rs.next() ) {
             System.out.println(rs.getString("film"));
@@ -59,7 +64,9 @@ if(x==2){
 	Scanner Sc1=new Scanner(System.in);
 	int y=Sc1.nextInt();
 	Statement stmt1 = con.createStatement();
-	rs = stmt1.executeQuery("SELECT Director FROM Film WHERE Director ="+y);      
+	rs = stmt1.executeQuery("SELECT Director FROM Film WHERE Director ="+y);   // так не будет работать
+																			   // ты вставл€ешь инфу о фильме с помощью динамического SQL
+	                                                                           // здесь тоже так нужно сделать - через prepareStatement
 }
 
      
